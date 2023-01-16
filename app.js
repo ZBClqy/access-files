da048a7b150ca11177389a5d56ffb3c917665a49
zhangsan <zhangsan@.com>
2023-01-16 21:41:00 +0800

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const app = express()
 const port = 3000
 const FilmRouter=require('./router/film.js')
 
+app.all("*",function(req,res,next){
+    // 设置允许跨域的域名,*代表允许任意域名跨域
+    res.header('Access-Control-Allow-Origin','*');
+    // 允许的header类型
+    res.header('Access-Control-Allow-Headers','content-type');
+    // 跨域允许的请求方式
+    res.header('Access-Control-Allow-Methods','DELETE,PUT,POST,GET,OPTIONS');
+    if(req.method.toLowerCase() == 'options')
+        res.send(200); // 让options 尝试请求快速结束
+    else
+        next();
+})
+
 app.use('/film',FilmRouter)
 
 app.listen(port, () => {
